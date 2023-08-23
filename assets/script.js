75fb1608bc86aa0aa484f47235d5956852a6acee
aoKel <128897820+MSeries-kelvin@users.noreply.github.com>
2023-08-23 16:46:41 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+// FOR SEARCH BAR
+let searchClick = false;
+let searchForm = document.querySelector(".user-info form");
+let searchIcon = document.querySelector(".user-info form i");
+let navIphone = document.querySelector(".nav ul li.iphone");
+document.querySelector("body").addEventListener("click", function() {
+    if (searchClick) {searchClick = false} else {
+        search.classList = "";
+        navIphone.style.display = "inline-block";
+        setTimeout(function() {
+            document.querySelector(".logo").style.display = "inline-block";
+            searchForm.classList = "";
+            searchIcon.classList = "fa-solid fa-magnifying-glass";
+        }, 400);
+        search.placeholder = "search";
+    }
+})
+let search = document.getElementById("search");
+search.addEventListener("click", function() {
+    searchClick = true;
+    search.classList = "active";
+    searchForm.classList = "active";
+    searchIcon.classList += " active";
+    if (window.innerWidth <= 1115 && window.innerWidth >= 1071) {
+        navIphone.style.display = "none";
+    }
+    if (window.innerWidth <= 750) {
+        document.querySelector(".logo").style.display = "none";
+    }
+    this.placeholder = "enter any phone name";
+});
+// END
+
+alert(window.innerWidth);
